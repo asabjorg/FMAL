a5d5eee62574ce5777c359817f83f1b5d5ee280f
Ása Björg Guðlaugsdóttir <asa13@ru.is>
2016-02-10 00:48:18 +0000

    bæta við klösum

--- src/assignment2/Compiler.java
+++ src/assignment2/Compiler.java
@@ -5,6 +5,11 @@ public class Compiler {
 	
 	public static void main(String[] args) {
 		System.out.println("HALLO");
+		
+		
+		/*Lexer myLexer = new Lexer();
+		Parser myParser = new Parser(myLexer);
+		myParser.parse();*/
 
 	}
 
--- src/assignment2/Token.java
+++ src/assignment2/Token.java
@@ -1,5 +1,12 @@
 package assignment2;
 
 public class Token {
+	
+	String lexeme;
+	TokenCode tCode;
+	
+	
+	enum TokenCode { ID, ASSIGN, SEMICOL, INT, ADD, SUB,
+	MULT, LPAREN, RPAREN, PRINT, END, ERROR }
 
 }
