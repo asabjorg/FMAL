a81af609ecd786ba23be6a0ee97a747bfb03f53f
Ása Björg Guðlaugsdóttir <asa13@ru.is>
2016-02-10 12:58:22 +0000

    Fylla inn í Lexer

--- src/assignment2/Lexer.java
+++ src/assignment2/Lexer.java
@@ -8,28 +8,54 @@ public class Lexer {
 		
 		System.out.println(input);
 		
+		
 		if(input.equals("{")){
 			return TokenCode.LPAREN;
 		}
-		
 		else if(input.equals("}")){
 			return TokenCode.RPAREN;
 		}
-		
 		else if(input.equals("+")){
 			return TokenCode.ADD;
 		}
-		
+		else if(input.equals("=")){
+			return TokenCode.ASSIGN;
+		}
 		else if(input.equals("*")){
 			return TokenCode.MULT;
 		}
 		else if(input.equals(";")){
 			return TokenCode.SEMICOL;
 		}
-	
+		else if(input.equals("-")){
+			return TokenCode.SUB;
+		}
+		else if(input.equals(";")){
+			return TokenCode.SUB;
+		}
+		else if(input.equals("end")){
+			return TokenCode.END;
+		}
+		else if(input.equals("print")){
+			return TokenCode.PRINT;
+		}
+		else if(isInt(input)){
+			return TokenCode.INT;
+		}
+		else if(isAlpha(input)){
+			return TokenCode.ID;
+		}
+			
 		return TokenCode.ERROR;
 		
+	}
+	
+	public static boolean isInt(String input) {
+	    return input.matches("[0-9]+");
+	}
 		
+	public static boolean isAlpha(String input) {
+	    return input.matches("[A-Za-z]+");
 	}
 	
 
