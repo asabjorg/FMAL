5c7b1bbe0bf453a6ed7f7295a175f88ed0579e91
Ása Björg Guðlaugsdóttir <asa13@ru.is>
2016-02-10 00:44:25 +0000

    setja a git

---
+++ src/assignment2/Compiler.java
@@ -0,0 +1,11 @@
+package assignment2;
+
+public class Compiler {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("HALLO");
+
+	}
+
+}
---
+++ src/assignment2/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ÁsaBjörg
+ *
+ */
+package assignment2;
